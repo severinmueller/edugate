7e499a6dd0bfebad3c5d434e5fda32bbf366064d
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-15 19:50:12 +0100

    database

--- application/config/database.php
+++ application/config/database.php
@@ -73,24 +73,27 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+
+$url = parse_url(getenv("DATABASE_URL"));
+
 $db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
+    'dsn'	=> '',
+    'hostname' => $url["host"],
+    'username' => $url["user"],
+    'password' => $url["pass"],
+    'database' => substr($url["path"], 1),
+    'dbdriver' => 'postgre',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
 );
--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,3 +1,7 @@
 
 Willkommen bei eduGate! Dein Tor zur Weiterbildung.
 
+$this->load->database();
+
+query = $this->db->get('news');
+return $query->result_array();
