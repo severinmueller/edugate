8980103735459a5f1a60faa0e096651399f705fd
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-17 18:17:33 +0100

    tutorial

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,8 +1,1 @@
-<?php
-
-echo "Willkommen bei eduGate! Dein Tor zur Weiterbildung.";
-
-$this->load->database();
-
-$query = $this->db->get('news');
-return $query->result_array();
+Hello World home
