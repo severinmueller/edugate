9431b77efdd72b9f6a0cea17a3d9958e6727f962
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-19 12:38:35 +0100

    delete news controller

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -36,7 +36,7 @@ class News_model extends CI_Model {
     public function delete_news($id)
     {
 
-        return $this->db->delete('news', $id);
+        $this->db->delete('news', array('id' => $id));
     }
 }
 
