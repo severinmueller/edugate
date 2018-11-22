9e7d0e5b2297ad3cbb54dd493352c12aefb7eebf
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-22 18:03:02 +0100

    logo update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,19 +6,20 @@
 <body>
 
 <nav class="navbar navbar-inverse">
-    <div class="container">
+    <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="/">ciBlog</a>
+            <a class="navbar-brand" href="#">WebSiteName</a>
         </div>
-        <div id="navbar">
-            <ul class="nav navbar-nav">
-                <li><a href="<?php echo base_url();?>">Home</a> </li>
-                <li><a href="<?php echo base_url()?>about">About</a> </li>
-            </ul>
-        </div>
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="/>Home</a></li>
+            <li><a href="<?php echo base_url();?>">Home</a> </li>
+            <li><a href="<?php echo base_url()?>about">About</a> </li>
+        </ul>
     </div>
-
-
 </nav>
 
-<h1><?php echo $title; ?></h1>
+
+
+
+
+
