cfb2030a31fb85ac09a070b03a7a51531a44bf46
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-22 19:27:57 +0100

    css update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -5,7 +5,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light py-1">
+<nav class="navbar navbar-expand-md navbar-light bg-light py-1">
     <a class="navbar-brand py-1" href="/"><img src=<?php echo base_url("assets/images/edugate-schrift.png");?> alt="edugate" height="80"></a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
