057d666130c505cc8a2b6809f4e4c16dcf59e522
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-22 18:05:25 +0100

    logo update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,14 +1,14 @@
 <html>
 <head>
     <title><?php echo $title; ?></title>
-    <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
 </head>
 <body>
 
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="#">WebSiteName</a>
+            <a class="navbar-brand" href="/">WebSiteName</a>
         </div>
         <ul class="nav navbar-nav">
             <li><a href="<?php echo base_url();?>">Home</a> </li>
