9c0b36032d5e0fff9456006beb387cb3143b1cf5
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-22 19:25:55 +0100

    css update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,6 +8,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light py-1">
     <a class="navbar-brand py-1" href="/"><img src=<?php echo base_url("assets/images/edugate-schrift.png");?> alt="edugate" height="80"></a>
 
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
     <div class="collapse navbar-collapse" id="navbarSuspportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item <?php if($this->uri->uri_string() == '') { echo 'active'; }?> py-1" >
