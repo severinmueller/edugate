aa6808cec1c3b3adbcc1e8dca27d841578c982f9
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-23 18:15:16 +0100

    posts create

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -1,1 +1,13 @@
 <h2><?php $title ?></h2>
+
+
+<form>
+    <div class="form-group">
+        <label>Title</label>
+        <input type="text" class="form-control" name="title" placeholder="Add title...">
+    </div>
+    <div class="form-group">
+        <label>Body</label>
+        <textarea class="form-control" name="body" placeholder="Add body..."></textarea>
+    </div>
+</form>
