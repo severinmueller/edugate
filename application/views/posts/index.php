168c90792e03eb789089a707520094fcf501cade
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-23 03:58:37 +0100

    posts read more link

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -2,5 +2,6 @@
 <h3><?php echo $post['title']; ?></h3>
 <small class="post-date">Posted on: <?php echo $post['created_at']; ?></small>
 <?php echo $post['body']; ?>
+<br>
     <p><a href="<?php echo site_url('posts/'.$post['slug']); ?>" Read more</a></p>
 <?php endforeach; ?>
