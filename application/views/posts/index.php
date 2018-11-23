2557e43a4822eaafcc43ea4205b88a53157d3ba7
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-23 02:05:44 +0100

    some css

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -1,5 +1,5 @@
 <?php foreach($posts as $post) : ?>
 <h3><?php echo $post['title']; ?></h3>
-<small>Posted on: <?php echo $post['created_at']; ?></small>
+<small class="post-date">Posted on: <?php echo $post['created_at']; ?></small>
 <?php echo $post['body']; ?>
 <?php endforeach; ?>
