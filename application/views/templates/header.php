6ba09ae18c6d58532a070ca9435e68d5a955327c
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-23 21:39:41 +0100

    posts delete form

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -24,6 +24,9 @@
             <li class="nav-item <?php if($this->uri->uri_string() == 'posts') { echo 'active'; }?> py-1">
                 <a class="nav-link py-1" href="<?php echo base_url();?>posts">Posts </a>
             </li>
+            <li class="nav-item <?php if($this->uri->uri_string() == 'posts/create') { echo 'active'; }?> py-1">
+                <a class="nav-link py-1" href="<?php echo base_url();?>posts/create">Create post </a>
+            </li>
         </ul>
     </div>
 </nav>
