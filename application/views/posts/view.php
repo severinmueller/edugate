2c3a9dee1a2b30d2fd6912cd2c49ee3ffb7e4df0
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-26 20:59:46 +0100

    posts edit, changes to get posts (slug instead of id)

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -7,7 +7,7 @@
 
 <hr>
 <?php echo form_open('posts/delete/'.$post['id']); ?>
-<a class="btn btn-default" href="edit/<?php echo $post['id'];?>">Edit</a>
+<a class="btn btn-default" href="edit/<?php echo $post['slug'];?>">Edit</a>
 <input type="submit" value="Delete" class="btn btn-danger">
 </form>
 
