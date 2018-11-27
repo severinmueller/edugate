1fbb9d61f1495828865a08a5e6b2ea229c0024f1
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-27 13:20:03 +0100

    categories bugfix

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -13,7 +13,7 @@
     </div>
 <div class="form-group">
     <label>Category</label>
-    <select name="category_id" class="form-control">
+    <select name="category" class="form-control">
         <?php foreach($categories as $category): ?>
             <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
         <?php endforeach; ?>
