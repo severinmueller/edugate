01bad3ec9c1fb5d11da31e0b4b8c0e3a938fa917
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-27 12:49:25 +0100

    categories

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -15,7 +15,7 @@
     <label>Category</label>
     <select name="category_id" class="form-control"></select>
     <?php foreach($categories as $category): ?>
-    <option value=""><?php echo $category['name'];?> </option>
+    <option value="<?php echo $category['id'];?>"><?php echo $category['name'];?></option>
     <?php endforeach; ?>
 </div>
 <button type="submit" class="btn btn-default">Submit</button>
