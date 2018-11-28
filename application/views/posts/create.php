97d1f9379c11529b6ab01482b3e9da239662f1a2
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-28 16:11:32 +0100

    image upload button

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -2,7 +2,7 @@
 
 <?php echo validation_errors(); ?>
 
-<?php echo form_open('posts/create'); ?>
+<?php echo form_open_multipart('posts/create'); ?>
     <div class="form-group">
         <label>Title</label>
         <input type="text" class="form-control" name="title" placeholder="Add title...">
@@ -18,6 +18,11 @@
             <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
         <?php endforeach; ?>
     </select>
+</div>
+
+<div class="form-group">
+    <label>Upload image</label>
+    <input type="file" name="postimage" size="20">
 </div>
 <button type="submit" class="btn btn-default">Submit</button>
 </form>
