b3b5d260ef6ef1bf11b0e8967b06bbf37b5e9fc6
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-28 18:25:25 +0100

    categories creation db model

--- application/controllers/Categories.php
+++ application/controllers/Categories.php
@@ -5,7 +5,7 @@
         public function  __construct()
         {
             parent::__construct();
-            $this->load->model('post_model');
+            $this->load->model('category_model');
             $this->load->helper('url_helper');
             $this->load->helper('form');
             $this->load->library('form_validation');
