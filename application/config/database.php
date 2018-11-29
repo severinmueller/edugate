c49ccf25439eb0680bdb77a3b9c0adb37a3cde98
Severin Müller <severin.mueller@students.fhnw.ch>
2018-11-29 15:29:00 +0100

    database config change

--- application/config/database.php
+++ application/config/database.php
@@ -82,7 +82,7 @@ $db['default'] = array(
     'username' => $url["user"],
     'password' => $url["pass"],
     'database' => substr($url["path"], 1),
-    'dbdriver' => 'postgre',
+    'dbdriver' => $url["scheme"],
     'dbprefix' => '',
     'pconnect' => FALSE,
     'db_debug' => (ENVIRONMENT !== 'production'),
