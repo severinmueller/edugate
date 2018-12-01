155d485953e451a37c96dcfb45d67fcc97b80624
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-01 22:47:21 +0100

    comments

--- application/models/Comment_model.php
+++ application/models/Comment_model.php
@@ -27,7 +27,7 @@ class Comment_model extends CI_Model{
 
 
     public function get_comments($post_id){
-            $query = $this->db->get_where('commnents',array('post_id' => $post_id));
+            $query = $this->db->get_where('comments',array('post_id' => $post_id));
             return $query->result_array();
     }
 }
