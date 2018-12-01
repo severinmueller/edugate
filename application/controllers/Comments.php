81213e880458f0dae7b138316023c5f7dfaf4791
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-01 23:44:21 +0100

    comments

--- application/controllers/Comments.php
+++ application/controllers/Comments.php
@@ -17,20 +17,17 @@
         public function create($post_id){
             $slug = $this->input->post('slug');
             $data['post'] = $this->post_model->get_posts($slug);
-
             $this->form_validation->set_rules('name', 'Name', 'required');
             $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
             $this->form_validation->set_rules('body', 'Body', 'required');
-
-            if($this->form_validation->run()===FALSE){
-                $this->load->view('templates/header', $data);
+            if($this->form_validation->run() === FALSE){
+                $this->load->view('templates/header');
                 $this->load->view('posts/view', $data);
-                $this->load->view('templates/footer', $data);
-            }else{
+                $this->load->view('templates/footer');
+            } else {
                 $this->comment_model->create_comment($post_id);
-                redirect('post/'.$slug);
+                redirect('posts/'.$slug);
             }
-
         }
 
 
--- application/models/Comment_model.php
+++ application/models/Comment_model.php
@@ -21,13 +21,10 @@ class Comment_model extends CI_Model{
             'email' => $this->input->post('email'),
             'body' => $this->input->post('body')
         );
-
         return $this->db->insert('comments', $data);
     }
-
-
     public function get_comments($post_id){
-            $query = $this->db->get_where('comments',array('post_id' => $post_id));
-            return $query->result_array();
+        $query = $this->db->get_where('comments', array('post_id' => $post_id));
+        return $query->result_array();
     }
 }
