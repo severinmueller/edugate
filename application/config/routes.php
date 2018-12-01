3a5fee42b9c5d314aeea201b251f49e95904d1c2
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-02 00:01:52 +0100

    posts create

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,22 +51,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 
-$route['categories'] = 'categories/index';
-$route['categories/create'] = 'categories/create';
-$route['categories/posts/(:any)'] = 'categories/posts/$1';
-
+$route['posts/index'] = 'posts/index';
 $route['posts/create'] = 'posts/create';
 $route['posts/update'] = 'posts/update';
 $route['posts/(:any)'] = 'posts/view/$1';
 $route['posts'] = 'posts/index';
-
-$route['404_override'] = 'errors/page_missing';
-$route['translate_uri_dashes'] = FALSE;
-
-$route['news/create'] = 'news/create';
-$route['news/delete/(:num)'] = 'news/delete/$1';
-$route['news/(:any)'] = 'news/view/$1';
-$route['news'] = 'news';
-
-$route['(:any)'] = 'pages/view/$1';
 $route['default_controller'] = 'pages/view';
+$route['categories'] = 'categories/index';
+$route['categories/create'] = 'categories/create';
+$route['categories/posts/(:any)'] = 'categories/posts/$1';
+$route['(:any)'] = 'pages/view/$1';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
