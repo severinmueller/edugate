232b6c9de98051dfb6b0b117037ae9b1255f3059
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-02 01:18:16 +0100

    data title

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -33,7 +33,7 @@ class Posts extends CI_Controller
         if(empty($data['post'])){
             show_404();
         }
-        $data['title'] = $this->post_model->get_posts($slug)->title;
+        $data['title'] = $data['post']['title'];
         $this->load->view('templates/header');
         $this->load->view('posts/view', $data);
         $this->load->view('templates/footer');
