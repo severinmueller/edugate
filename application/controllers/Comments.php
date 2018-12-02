927f49d72033a485aa193cef0e69429da6a2007a
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-02 01:02:15 +0100

    data title

--- application/controllers/Comments.php
+++ application/controllers/Comments.php
@@ -17,6 +17,7 @@
         public function create($post_id){
             $slug = $this->input->post('slug');
             $data['post'] = $this->post_model->get_posts($slug);
+            $data['title'] = $data['post']['title'];
             $this->form_validation->set_rules('name', 'Name', 'required');
             $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
             $this->form_validation->set_rules('body', 'Body', 'required');
