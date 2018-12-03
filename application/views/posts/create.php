e91c8f057a1314f5eb4ce36fc178757d184382cf
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-04 00:58:03 +0100

    ckeditor configs

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -12,28 +12,27 @@
         <textarea name="body" id="editor"></textarea>
         <script>
 
-            CKEDITOR.replace( 'editor' )
-                .editorConfig = function( config ) {
-                    config.toolbarGroups = [
-                        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-                        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-                        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-                        { name: 'forms', groups: [ 'forms' ] },
-                        '/',
-                        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-                        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-                        { name: 'links', groups: [ 'links' ] },
-                        { name: 'insert', groups: [ 'insert' ] },
-                        '/',
-                        { name: 'styles', groups: [ 'styles' ] },
-                        { name: 'colors', groups: [ 'colors' ] },
-                        { name: 'tools', groups: [ 'tools' ] },
-                        { name: 'others', groups: [ 'others' ] },
-                        { name: 'about', groups: [ 'about' ] }
-                    ];
+            CKEDITOR.replace( 'editor1', {
+                toolbarGroups:
+                    {name: 'document', groups: ['mode', 'document', 'doctools']},
+                    {name: 'clipboard', groups: ['clipboard', 'undo']},
+                    {name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing']},
+                    {name: 'forms', groups: ['forms']},
+                    '/',
+                    {name: 'basicstyles', groups: ['basicstyles', 'cleanup']},
+                    {name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph']},
+                    {name: 'links', groups: ['links']},
+                    {name: 'insert', groups: ['insert']},
+                    '/',
+                    {name: 'styles', groups: ['styles']},
+                    {name: 'colors', groups: ['colors']},
+                    {name: 'tools', groups: ['tools']},
+                    {name: 'others', groups: ['others']},
+                    {name: 'about', groups: ['about']}
+                ],
 
-                    config.removeButtons = 'NewPage,Save,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,RemoveFormat,CopyFormatting,Outdent,Indent,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,Image,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,Format,Font,FontSize,TextColor,BGColor,Maximize,ShowBlocks,About';
-                };
+                removeButtons: 'NewPage,Save,Preview,Print,Templates,Cut,Copy,Paste,PasteText,PasteFromWord,Find,Replace,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,RemoveFormat,CopyFormatting,Outdent,Indent,CreateDiv,BidiLtr,BidiRtl,Language,Anchor,Image,Flash,Table,HorizontalRule,Smiley,SpecialChar,PageBreak,Iframe,Styles,Format,Font,FontSize,TextColor,BGColor,Maximize,ShowBlocks,About';
+            });
 
         </script>
     </div>
