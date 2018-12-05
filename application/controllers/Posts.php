eb249252cd29ea17ce2dbf7c572cd2db2b888da8
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-05 23:08:23 +0100

    access control

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -42,35 +42,42 @@ class Posts extends CI_Controller
 
     public function create(){
         // Check login
-        $data['title'] = 'Create Post';
-        $data['categories'] = $this->post_model->get_categories();
-        $this->form_validation->set_rules('title', 'Title', 'required');
-        $this->form_validation->set_rules('body', 'Body', 'required');
-        if($this->form_validation->run() === FALSE){
-            $this->load->view('templates/header',$data);
-            $this->load->view('posts/create', $data);
-            $this->load->view('templates/footer');
-        } else {
-            // Upload Image
-            $config['upload_path'] = './assets/images/posts';
-            $config['allowed_types'] = 'gif|jpg|png';
-            $config['max_size'] = '2048';
-            $config['max_width'] = '2000';
-            $config['max_height'] = '2000';
-            $this->load->library('upload', $config);
-            if(!$this->upload->do_upload()){
-                $errors = array('error' => $this->upload->display_errors());
-                $post_image = 'noimage.jpg';
+        if($this->session->userdata('logged_in')) {
+            $data['title'] = 'Create Post';
+            $data['categories'] = $this->post_model->get_categories();
+            $this->form_validation->set_rules('title', 'Title', 'required');
+            $this->form_validation->set_rules('body', 'Body', 'required');
+            if($this->form_validation->run() === FALSE){
+                $this->load->view('templates/header',$data);
+                $this->load->view('posts/create', $data);
+                $this->load->view('templates/footer');
             } else {
-                $data = array('upload_data' => $this->upload->data());
-                $post_image = $_FILES['userfile']['name'];
+                // Upload Image
+                $config['upload_path'] = './assets/images/posts';
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size'] = '2048';
+                $config['max_width'] = '2000';
+                $config['max_height'] = '2000';
+                $this->load->library('upload', $config);
+                if(!$this->upload->do_upload()){
+                    $errors = array('error' => $this->upload->display_errors());
+                    $post_image = 'noimage.jpg';
+                } else {
+                    $data = array('upload_data' => $this->upload->data());
+                    $post_image = $_FILES['userfile']['name'];
+                }
+                $this->post_model->create_post($post_image);
+
+                $this->session->set_flashdata('post_created', 'Your post has been created.');
+
+                redirect('posts');
             }
-            $this->post_model->create_post($post_image);
-
-            $this->session->set_flashdata('post_created', 'Your post has been created.');
-
-            redirect('posts');
+        }else{
+            redirect('users/login');
         }
+
+
+
     }
     public function delete($id){
         $this->post_model->delete_post($id);
