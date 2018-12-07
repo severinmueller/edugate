5e0802c5732bb89af6c15ce302b34c950ff923c1
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-07 22:49:16 +0100

    redesign

--- application/views/courses/index.php
+++ application/views/courses/index.php
@@ -14,7 +14,7 @@
         <tr>
             <td><?php echo $course['title']; ?></td>
             <td><a class="btn btn-outline-dark" href="<?php echo site_url('/courses/'.$course['slug']); ?>">Read More</a></td>
-            <td><?php echo $course['users.name']; ?></td>
+            <td><?php echo $course['name']; ?></td>
             <td>29.99</td>
         </tr>
         <?php endforeach; ?>
