8a343ae01e6048565a6af068a0392e26a57c32a1
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-07 20:34:39 +0100

    redesign

--- application/views/courses/manage.php
+++ application/views/courses/manage.php
@@ -1,1 +1,10 @@
-<b>hallo</b>
+<?php foreach($courses as $course) : ?>
+    <h3><?php echo $course['title']; ?></h3>
+    <small ><?php echo $course['name'];?></small>
+    <?php echo html_purify(word_limiter($course['body'], 50)); ?>
+    <br>
+    <p><a class="btn btn-outline-dark" href="<?php echo site_url('/courses/'.$course['slug']); ?>">Read More</a></p>
+<?php endforeach; ?>
+
+
+
