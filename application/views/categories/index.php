e0b16d70c6cc41d4419171dd5d59feb3a6548e46
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-07 21:02:45 +0100

    redesign

--- application/views/categories/index.php
+++ application/views/categories/index.php
@@ -1,7 +1,14 @@
 <ul class="list-group">
 
+    <?php $style = 0; ?>
     <?php foreach ($categories as $category) : ?>
-
+    <?php if($style<11){
+        $style++;
+    }else {
+            $style = 0;
+        }
+    ?>
         <li class="list-group-item"><a href="<?php echo site_url('/categories/courses/'.$category['id']); ?>"><?php echo $category['name']; ?></a></li>
+   <?php echo "style".$style ?>
     <?php endforeach; ?>
 </ul>
