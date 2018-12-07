f3ba783190bbf65dc83728ba56afef95f6452377
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-07 23:03:56 +0100

    redesign

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -5,7 +5,8 @@
             <h1 class="text-center"><?php echo $title; ?></h1>
                 <input type="text" name="username" class="form-control" placeholder="Enter username" required autofocus>
                 <input type="password" name="password" class="form-control" placeholder="Enter password" required autofocus>
-            <button type="submit" class="btn btn-success">Login</button>
+        <br>
+            <button type="submit">Login</button>
         <?php echo form_close(); ?>
 
     </div>
