5ffe55322a2e9af6650c4ff4c9536be0c24c20a4
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-07 21:37:57 +0100

    redesign

--- application/views/courses/index.php
+++ application/views/courses/index.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 2
- * Date: 06.12.2018
- * Time: 21:57
- */
+<h2>Table</h2>
+<h3>Default</h3>
+<div class="table-wrapper">
+    <table>
+        <thead>
+        <tr>
+            <th>Kurs</th>
+            <th>Anbieter</th>
+            <th>Ort</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach($courses as $course) : ?>
+        <tr>
+            <td><?php echo $course['title']; ?></td>
+            <td><a class="btn btn-outline-dark" href="<?php echo site_url('/courses/'.$course['slug']); ?>">Read More</a></td>
+            <td>29.99</td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</div>
+
+
