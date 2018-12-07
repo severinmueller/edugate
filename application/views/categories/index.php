e51e58194724e45892d8b4a793b7d846ddb98285
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-07 21:06:08 +0100

    redesign

--- application/views/categories/index.php
+++ application/views/categories/index.php
@@ -1,4 +1,10 @@
-<ul class="list-group">
+<div id="main">
+    <div class="inner">
+        <header>
+            <h1>Dein Portal zur Weiterbildung.</h1>
+            <p>Entdecke jetzt das vielfältige Angebot an Weiterbildungskursen von Fachhoschulen in der ganzen Schweiz. Wähle die Kategorie, für die Du dich interessierts:</p>
+        </header>
+
 
     <?php $style = 0; ?>
     <?php foreach ($categories as $category) : ?>
@@ -8,7 +14,20 @@
             $style = 0;
         }
     ?>
-        <li class="list-group-item"><a href="<?php echo site_url('/categories/courses/'.$category['id']); ?>"><?php echo $category['name']; ?></a></li>
-   <?php echo "style".$style ?>
+
+        <section class="tiles">
+            <article class=" <?php echo "style".$style ?>">
+									<span class="image">
+										<img src="images/pic05.jpg" alt="" />
+									</span>
+                <a href="<?php echo site_url('/categories/courses/'.$category['id']); ?>"></a>
+                <h2><?php echo $category['name']; ?></h2>
+                <div class="content">
+                    <p></p>
+                </div>
+                </a>
+            </article>
     <?php endforeach; ?>
-</ul>
+
+
+
