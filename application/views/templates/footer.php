3bf512d15aadd69f874d932705e7df12eac4b83b
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-07 18:19:14 +0100

    redesign

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -42,7 +42,7 @@
 </div>
 
 <!-- Scripts -->
-<script src="<?php echo base_url('assets/js/jquery.min.js)')?></script>
+<script src="<?php echo base_url('assets/js/jquery.min.js')?></script>
 <script src="<?php echo base_url('assets/js/browser.min.js')?>></script>
 <script src="<?php echo base_url('assets/js/breakpoints.min.js')?>></script>
 <script src="<?php echo base_url('assets/js/util.js')?>></script>
