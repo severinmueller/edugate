72299c7303860163b2193385a4a26d0c0c993f38
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-07 21:15:49 +0100

    redesign

--- application/views/categories/index.php
+++ application/views/categories/index.php
@@ -14,11 +14,9 @@
             $style = 0;
         }
     ?>
-
-
             <article class="<?php echo "style".$style ?>">
 									<span class="image">
-										<img src="images/pic05.jpg" alt="" />
+										<img src="base_url(assets/images/pic05.jpg" alt="" />
 									</span>
                 <a href="<?php echo site_url('/categories/courses/'.$category['id']); ?>">
                 <h2><?php echo $category['name']; ?></h2>
