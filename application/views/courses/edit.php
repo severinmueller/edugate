01de6fd5af2e7d64722469bf9667394691ac141c
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-08 20:07:05 +0100

    edit course

--- application/views/courses/edit.php
+++ application/views/courses/edit.php
@@ -1,7 +1,34 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 2
- * Date: 06.12.2018
- * Time: 21:57
- */
+<h2><?php $title ?></h2>
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('courses/update/'.$course['id']); ?>
+<div class="form-group">
+    <label>Title</label>
+    <input type="text" class="form-control" name="title" placeholder="Add title..." value="<?php echo $course['title']?>">
+</div>
+<div class="form-group">
+    <label>Body</label>
+    <textarea name="body" id="editor"><?php echo html_purify($course['body'])?></textarea>
+    <script>
+
+        CKEDITOR.replace( 'editor', {
+            customConfig: '/assets/js/CKEditConfigs.js'
+        });
+
+    </script>
+</div>
+
+</div>
+<button type="submit" class="button">Submit</button>
+
+<div class="form-group">
+    <label>Category</label>
+    <select name="category" class="form-control">
+        <?php foreach($categories as $category): ?>
+            <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
+        <?php endforeach; ?>
+    </select>
+</div>
+</form>
+
