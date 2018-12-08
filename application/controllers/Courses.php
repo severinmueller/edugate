4f129007d755cf97a4f402c12debb8f5cbf25080
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-08 02:39:01 +0100

    mail

--- application/controllers/Courses.php
+++ application/controllers/Courses.php
@@ -35,7 +35,7 @@ $this->email->initialize(array(
 ));
 
 $this->email->from('app115613062@heroku.com');
-$this->email->to('severin.students.fhnw.ch');
+$this->email->to('severin.mueller@students.fhnw.ch');
 $this->email->subject('Email Test');
 $this->email->message('Testing the email class.');
 $this->email->send();
