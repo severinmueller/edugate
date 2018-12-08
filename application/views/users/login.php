e7c261e26f3e21e82a59eb33f24a1c17273c7643
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-08 22:27:07 +0100

    login

--- application/views/users/login.php
+++ application/views/users/login.php
@@ -5,6 +5,7 @@
             <h1 class="text-center"><?php echo $title; ?></h1>
                 <input type="text" name="username" class="form-control" placeholder="Enter username" required autofocus>
                 <input type="password" name="password" class="form-control" placeholder="Enter password" required autofocus>
+        <br>
                 <input type="checkbox" id="rememberme" name="rememberme"> <label for="remember me">Angemeldet bleiben</label>
         <br>
             <button type="submit">Login</button>
