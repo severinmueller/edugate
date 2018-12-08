c75cc5cec32cd30b2155cac752712228d9ede056
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-08 19:34:45 +0100

    manage courses

--- application/views/courses/manage.php
+++ application/views/courses/manage.php
@@ -23,7 +23,7 @@
                         <td><?php echo $course['name']; ?></td>
                         <td><?php echo $course['start_date']; ?></td>
                         <td><a class="button" href="edit/<?php echo $course['slug'];?>">Bearbeiten</a></td>
-                        <td><?php echo form_open('courses/delete/'.$course['id']); ?> <input type="submit" value="Löschen" class="button"></form></td>
+                        <td><a class="button" href="delete/<?php echo $course['id'];?>">Löschen</a></td>
                     </tr>
                 <?php endforeach; ?>
             </table>
