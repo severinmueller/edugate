46e9366780e4e320a400c35134f5a5ea02896f4b
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-08 02:17:57 +0100

    mail

--- application/config/email.php
+++ application/config/email.php
@@ -4,5 +4,5 @@ $config['smtp_host'] = 'smtp.sendgrid.net';
 $config['smtp_user'] = getenv("SENDGRID_USERNAME");
 $config['smtp_password'] = getenv("SENDGRID_PASSWORD");
 $config['smtp_port'] = 587;
-$config['crlf'] = '\r\n';
-$config['newline'] = '\r\n';
+$config['crlf'] = "\r\n";
+$config['newline'] = "\r\n";
