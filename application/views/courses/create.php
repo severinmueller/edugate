778f03e839061569e65f37ceb0de2add4cec733b
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-08 19:42:06 +0100

    create course

--- application/views/courses/create.php
+++ application/views/courses/create.php
@@ -1,7 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 2
- * Date: 06.12.2018
- * Time: 21:57
- */
+<h2><?php $title ?></h2>
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open_multipart('courses/create'); ?>
+<div class="form-group">
+    <label>Title</label>
+    <input type="text" class="form-control" name="title" placeholder="Add title...">
+</div>
+<div class="form-group">
+    <label>Body</label>
+    <textarea name="body" id="editor"></textarea>
+    <script>
+
+        CKEDITOR.replace( 'editor', {
+            customConfig: '/assets/js/CKEditConfigs.js'
+        });
+
+    </script>
+</div>
+<div class="form-group">
+    <label>Category</label>
+    <select name="category" class="form-control">
+        <?php foreach($categories as $category): ?>
+            <option value="<?php echo $category['id']; ?>"><?php echo $category['name']; ?></option>
+        <?php endforeach; ?>
+    </select>
+</div>
+
+<button type="submit" class="button">Submit</button>
+</form>
