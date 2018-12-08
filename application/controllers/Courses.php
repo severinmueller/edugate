ac9043273853105a96d3aa6daf1cedd8e54caf49
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-08 02:29:31 +0100

    mail

--- application/controllers/Courses.php
+++ application/controllers/Courses.php
@@ -28,13 +28,14 @@ $this->email->initialize(array(
   'protocol' => 'smtp',
   'smtp_host' => 'smtp.sendgrid.net',
   'smtp_user' => getenv("SENDGRID_USERNAME"),
-  'smtp_pass' => getenv("SENDGRID_password"),
+  'smtp_pass' => getenv("SENDGRID_PASSWORD"),
   'smtp_port' => 587,
   'crlf' => "\r\n",
   'newline' => "\r\n"
 ));
 
-$this->email->from(getenv("SENDGRID_USERNAME"));
+$this->email->from('severin.students.fhnw.ch');
+$this->email->to('severin.students.fhnw.ch');
 $this->email->subject('Email Test');
 $this->email->message('Testing the email class.');
 $this->email->send();
