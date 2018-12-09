c7fca2b7216c9dc78c8cceb190e0f3e197f43c4e
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-09 19:29:37 +0100

    reset password

--- application/views/users/reset/form.php
+++ application/views/users/reset/form.php
@@ -3,4 +3,3 @@ echo "reset";
 
 
 echo bin2hex(random_bytes(20));
-echo $date;
--- application/views/users/reset/newpassword.php
+++ application/views/users/reset/newpassword.php
@@ -2,7 +2,7 @@
 echo "newpassword";
 echo $token1;
 ?>
-        <?php echo form_open('users/login'); ?>
+        <?php echo form_open('users/reset'); ?>
    <div class="form-group">
     <label>Neues Passwort</label>
     <input type="password" class="form-control" name="password" placeholder="Passwort">
