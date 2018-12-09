1f3e3817f1b22f33202552bea36f446e5813ca48
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-09 21:39:49 +0100

    htmp purify

--- application/views/courses/view.php
+++ application/views/courses/view.php
@@ -1,10 +1,10 @@
         <hr>
-<h2><?php echo $course['title']; ?></h2>
+<h2><?php echo html_purify($course['title']); ?></h2>
 
         <ul class="alt">
-            <li><b>Anbieter:</b> <?php echo $course['name']; ?></li>
-            <li><b>Ort:</b> <?php echo $course['location']; ?></li>
-            <?php if (!empty($course['start_date'])): ?><li><b>Startdatum:</b> <?php echo $course['start_date']; ?></li> <?php endif; ?>
+            <li><b>Anbieter:</b> <?php echo html_purify($course['name']); ?></li>
+            <li><b>Ort:</b> <?php echo html_purify($course['location']); ?></li>
+            <?php if (!empty($course['start_date'])): ?><li><b>Startdatum:</b> <?php echo html_purify($course['start_date']); ?></li> <?php endif; ?>
             <li><b>Beschreibung:</b></li>
         </ul>
 <div class=post-body">
--- application/views/users/login.php
+++ application/views/users/login.php
@@ -3,7 +3,7 @@
                 <input type="text" name="email" class="form-control" placeholder="E-Mail-Adresse" required autofocus>
                 <input type="password" name="password" class="form-control" placeholder="Passwort" required autofocus>
         <br>
-            <button type="submit">Login</button> <a href="<?php echo site_url('/users/reset'); ?>">Passwort vergessen?</a>
+            <button type="submit">Login</button>&nbsp; &nbsp;<a href="<?php echo site_url('/users/reset'); ?>">Passwort vergessen?</a>
         <?php echo form_close(); ?>
 
     </div>
