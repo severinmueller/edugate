b967a326640ce149bc64cabac5d92da6d419f242
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-10 15:22:07 +0100

    styling

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -8,11 +8,11 @@
                 <li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
                 <li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
                 <li><a href="#" class="icon style2 fa-phone"><span class="label">Phone</span></a></li>
-                <li><a href="webmaster@edugate-ch.herokuapp.com" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
+                <li><a email="webmaster@edugate.ch class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
             </ul>
         </section>
         <ul class="copyright">
-            <li>&copy; Untitled. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+            <li>&copy; edugate. All rights reserved</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
         </ul>
     </div>
 </footer>
