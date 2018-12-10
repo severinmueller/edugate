6254dc22e699c0bc6ce8d37446d9b141394dd1ad
Severin Müller <severin.mueller@students.fhnw.ch>
2018-12-10 18:27:43 +0100

    send invoice

--- application/models/Course_model.php
+++ application/models/Course_model.php
@@ -74,10 +74,17 @@ class Course_model extends CI_Model
             'newline' => "\r\n"
         ));
 
+        $user_id =  $this->session->userdata('user_id');
+        $this->db->where('id',$user_id);
+        $result = $this->db->get('users');
+        $email =  $result->row(0)->email;
+        $coursetitle =  $this->input->post('title');
+        $invoicenumber = rand(0,999999999);
+
         $this->email->from('edugate@sendgrid.me');
-        $this->email->to('severin.mueller@students.fhnw.ch');
-        $this->email->subject('Reset link');
-        $this->email->message('Dein Link zum Passwort-Reset: '.$link);
+        $this->email->to($email);
+        $this->email->subject('Rechnung - Kursanzeige für '.$coursetitle);
+        $this->email->message('Guten Tag, \n Sie haben eine Kursanzeige gekauft für den Kurs '.$coursetitle.' gekauft. \n Kosten: 50 CHF. \n Bitte begleichen Sie die Rechnung unter Angabe der Referenz '.$invoicenumber.' auf das Konto XXXXXXXX. \n \n Freundliche Grüsse \n edugate');
         $this->email->send();
 
 
